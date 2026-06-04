ffcb1c7b630223e7c0c24623da5bd3baeaddcd96
Franco Atme <i202507429@cibertec.edu.pe>
2026-06-03 22:13:34 -0500

    Creación del Proyecto

---
+++ src/main/java/com/ciberfarma/ProyectoLp2GrupoAApplication.java
@@ -0,0 +1,13 @@
+package com.ciberfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLp2GrupoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLp2GrupoAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciberfarma/ProyectoLp2GrupoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLp2GrupoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
